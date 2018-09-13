3c32c82e0dafe4503456717a9566bee0c436f41e
Timothy Brennan <2timothy215@gmail.com>
2018-09-13 15:01:27 -0400

    NPM, gulp and sass in doc root updated gitignore

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const groupmq = require('gulp-group-css-media-queries');
+const bs = require('browser-sync');
+
+const SASS_SOURCES = [
+  './*.scss', // This picks up our style.scss file at the root of the theme
+  'css/**/*.scss', // All other Sass files in the /css directory
+];
+
+/**
+ * Compile Sass files
+ */
+gulp.task('compile:sass', () =>
+  gulp.src(SASS_SOURCES, { base: './' })
+    .pipe(plumber()) // Prevent termination on error
+    .pipe(sass({
+      indentType: 'tab',
+      indentWidth: 1,
+      outputStyle: 'expanded', // Expanded so that our CSS is readable
+    })).on('error', sass.logError)
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      })
+    ]))
+    .pipe(groupmq()) // Group media queries!
+    .pipe(gulp.dest('.')) // Output compiled files in the same dir as Sass sources
+    .pipe(bs.stream())); // Stream to browserSync
