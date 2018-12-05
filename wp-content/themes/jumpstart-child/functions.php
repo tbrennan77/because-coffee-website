9fd3cb122b248f30eeaeb4f9b3336f4beaee0bbb
Timothy Brennan <2timothy215@gmail.com>
2018-12-05 09:51:45 -0500

    Header done

--- wp-content/themes/jumpstart-child/functions.php
+++ wp-content/themes/jumpstart-child/functions.php
@@ -21,3 +21,10 @@ function jumpstart_option_id( $id ) {
     return 'jumpstart';
 }
 add_filter('themeblvd_option_id', 'jumpstart_option_id');
+
+// enqueue scripts
+function custom_scripts() {
+	wp_register_script('brew', get_stylesheet_directory_uri() . '/js/brew.js', array('jquery'),'1.1', true);
+	wp_enqueue_script('brew');
+}
+add_action('wp_enqueue_scripts', 'custom_scripts'); 
