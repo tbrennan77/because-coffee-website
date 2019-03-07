1eb4a9e248c6d670de908b9d72eac7a090bd95eb
Timothy Brennan <2timothy215@gmail.com>
2019-03-07 15:47:08 -0500

    Update WooCommerce template after plugin update

--- wp-content/themes/jumpstart-child/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/jumpstart-child/woocommerce/single-product/add-to-cart/variable.php
@@ -12,30 +12,33 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.4.1
+ * @version 3.5.5
  */
 
 defined( 'ABSPATH' ) || exit;
 
 global $product;
 
-$attribute_keys = array_keys( $attributes );
+$attribute_keys  = array_keys( $attributes );
+$variations_json = wp_json_encode( $available_variations );
+$variations_attr = function_exists( 'wc_esc_json' ) ? wc_esc_json( $variations_json ) : _wp_specialchars( $variations_json, ENT_QUOTES, 'UTF-8', true );
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-<form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ); // WPCS: XSS ok. ?>">
+<form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo $variations_attr; // WPCS: XSS ok. ?>">
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
 		<p class="stock out-of-stock"><?php esc_html_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
 	<?php else : ?>
-
-		<div class="variations">
+		<table class="variations" cellspacing="0">
+			<tbody>
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
-					<div>
-						<label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></td>
-					</div>
-					<div>
+					<tr>
+						<td class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></td>
+					</tr>
+					<tr>
+						<td class="value">
 							<?php
 								wc_dropdown_variation_attribute_options( array(
 									'options'   => $options,
@@ -44,9 +47,11 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 								) );
 								echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) ) : '';
 							?>
-					</div>
+						</td>
+					</tr>
 				<?php endforeach; ?>
-		</div>
+			</tbody>
+		</table>
 
 		<div class="single_variation_wrap">
 			<?php
